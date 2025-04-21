1301b11e1df71906a8f37ffa0f77483118add19d
Nicholas Sily <Nicholas.svasc@gmail.com>
2025-04-21 09:45:19 -0400

    Add Triangulation With Additional Info Struct

---
+++ FaceInfo.h
@@ -0,0 +1,6 @@
+struct FaceInfo
+{
+    int id;
+    FaceInfo() : id(-1) {} // Required: DefaultConstructible
+    FaceInfo(int i) : id(i) {}
+};
--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Triangulation_2.h>
+#include <CGAL/Triangulation_face_base_with_info_2.h>
+#include <CGAL/Triangulation_vertex_base_2.h>
+#include <CGAL/Triangulation_data_structure_2.h>
 #include <CGAL/draw_triangulation_2.h>
+#include "FaceInfo.h"
 #include <fstream>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Triangulation_2<K> Triangulation;
+typedef CGAL::Triangulation_vertex_base_2<K> Vb;
+typedef CGAL::Triangulation_face_base_with_info_2<FaceInfo, K> Fb;
+typedef CGAL::Triangulation_data_structure_2<Vb, Fb> Tds;
+typedef CGAL::Triangulation_2<K, Tds> Triangulation;
 typedef Triangulation::Point Point;
 
 int main(int argc, char *argv[])
@@ -17,6 +24,13 @@ int main(int argc, char *argv[])
 
     Triangulation t;
     t.insert(points.begin(), points.end());
+
+    // Iterate all faces and set ID for each
+    int currId = 0;
+    for (auto faceIt = t.finite_faces_begin(); faceIt != t.finite_faces_end(); faceIt++)
+    {
+        faceIt->info().id = currId++;
+    }
 
     CGAL::draw(t);
 
